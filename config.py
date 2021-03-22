76d83314910923a3178f03d7473e118d4f58ace0
Deleted-accounts <74473446+Deleted-accounts@users.noreply.github.com>
2021-03-22 15:22:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 
 # required
-TOKEN = ""
+TOKEN = "" # from @BotFather
 cretorBt = "למעבר ליוצר ⚔"
-creator_link = "YOUR USERNAME"
+creator_link = "YOUR USERNAME" # or @Usernae or start with https://t.me/
+
+# create a new channel and paste the id (get it from @userinfobot)
 LOG_CHANNEL = -100
 
 # optional
-bt1 = ""
-bt2 = ""
-bt1url = ""
-bt2url = ""
+bt1 = "" # text for new button
+bt2 = "" # text for new button
+bt1url = "" # url for new button1
+bt2url = "" # url for new button2
